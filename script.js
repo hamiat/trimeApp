78605b1357d24bbe25d4440d0d953e0baf182752
hamiat <hamiat.n@hotmail.com>
2021-03-11 10:13:56 +0100

    css and js completed

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let footerBody = document.getElementById("footerBody");
 hamMenuBtn.addEventListener("click", expandMenu);
 
 function expandMenu() {
-  if (hamMenu.style.display === "none") {
+  if (hamMenu.style.display == "none" || hamMenu.style.display == "" ) {
     hamMenu.style.display = "grid";
     mainBody.style.opacity = "0.5";
     footerBody.style.opacity = "0.5";
